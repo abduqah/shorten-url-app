8811a2725315f2a23bb9fc6ec708ea3cfd1dfb87
Abdurrahman <abduqah@gmail.com>
2020-10-08 22:50:26 +0200

    Modified routes, add urls create and original

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :urls, only: :create do
+        collection do
+          post 'original'
+        end
+      end
+    end
+  end
 end
